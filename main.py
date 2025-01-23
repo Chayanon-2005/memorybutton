68727ad897f3ecf1db1083cf26299287848b7d35
Chayanon-2005 <chayanon0615524253@gmail.com>
2025-01-24 06:03:47 +0700

    Making Layout

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ from kivy.uix.button import Button
 from random import shuffle
 
 class SequenceGameApp(App):
+    def build(self):
+        self.root_layout = GridLayout(cols=5, spacing=10, padding=10)   
