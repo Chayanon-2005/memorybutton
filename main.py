87e448dc4233d50726745c98e89dfe9e263ff3e9
Chayanon-2005 <chayanon0615524253@gmail.com>
2025-01-24 06:22:48 +0700

    Random number 1-30

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ class SequenceGameApp(App):
         self.root_layout = GridLayout(cols=5, spacing=10, padding=10)
         #สร้าง 30 ปุ่ม
         self.buttons = []
-        self.sequence = list(range(1, 31))   
+        self.sequence = list(range(1, 31)) 
+        shuffle(self.sequence)  # สุ่มลำดับของปุ่ม
+        self.current_index = 1 #โดยให้เริ่มจาก 1
+
 
 if __name__ == "__main__":
     SequenceGameApp().run()
