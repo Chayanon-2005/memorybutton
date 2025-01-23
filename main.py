8cd83f829f60b957b5bdfdd8a2e0f467d101a6a2
Chayanon-2005 <chayanon0615524253@gmail.com>
2025-01-24 06:17:32 +0700

    Making 30 Buttons

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from random import shuffle
 
 class SequenceGameApp(App):
     def build(self):
-        self.root_layout = GridLayout(cols=5, spacing=10, padding=10)   
+        #สร้าง layout
+        self.root_layout = GridLayout(cols=5, spacing=10, padding=10)
+        #สร้าง 30 ปุ่ม
+        self.buttons = []
+        self.sequence = list(range(1, 31))   
 
 if __name__ == "__main__":
     SequenceGameApp().run()
